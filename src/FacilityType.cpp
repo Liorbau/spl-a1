2bc6d0a1d76c362949a4e857b3a5e50fb73156f7
Omer Bar <omerbar12345@gmail.com>
2024-12-06 00:08:45 +0000

    fixed bugs and added backup & restore

--- src/FacilityType.cpp
+++ src/FacilityType.cpp
@@ -4,10 +4,6 @@
     FacilityType::FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score)
     : name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), economy_score(economy_score), environment_score(environment_score){}
     
-    //Copy constructor
-    FacilityType::FacilityType(const FacilityType &other)
-    : name(other.name), category(other.category), price(other.price), lifeQuality_score(other.lifeQuality_score), economy_score(other.economy_score), environment_score(other.environment_score) {}
-
     //Methods
     const string& FacilityType::getName() const {
         return name;
